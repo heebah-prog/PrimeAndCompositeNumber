062d839dce534294121d5361872c4ec814accbae
heebah-prog <102908701+heebah-prog@users.noreply.github.com>
2022-05-19 16:26:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,36 @@
 
-def isNumberValid(num):
+def isInputValid(num):
     if(num <= 1):
-        return -1 // Number not Valid
+        return -1
     else:
         return num
 
 # print(isNumberValid(1))
 
-def primeChecker(num):
-    isPrime = True
+def primeCheckerMethod1(num):
+    isPrime = False
+    iterationForPrime = 0
+
     for x in range(2,num):
         result = num%x
+        iterationForPrime += 1
         if(result == 0):
             isPrime = False
             break
         else:
             isPrime = True
 
-    print(isPrime)
+    return isPrime, iterationForPrime
 
-# primeChecker(5)
+
+def getFactorsOfCompositeNumber(num):
+    factors= []
+    iterationForComposite = 0
+    for x in range(2, num):
+        iterationForComposite +=1
+        if(num%x == 0):
+            factors.append(x)
+        else:
+            pass
+    return factors, iterationForComposite
+
