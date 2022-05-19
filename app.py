a92c5887bce6b596ecfd4dd8545f06fdb52896d6
heebah-prog <102908701+heebah-prog@users.noreply.github.com>
2022-05-19 16:35:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -34,3 +34,24 @@ def getFactorsOfCompositeNumber(num):
             pass
     return factors, iterationForComposite
 
+
+
+# ***************************************
+#            APPLICATION ENTRY
+# ***************************************
+
+userInput = int(input("\nPlease give a number -> "))
+
+isPrime, iterationForPrime = primeCheckerMethod1(userInput)
+factors, iterationForComposite = getFactorsOfCompositeNumber(userInput)
+
+if(isInputValid(userInput) != -1):
+    if(isPrime == True):
+        print("\n"+ str(userInput) +" is prime number and factors are -> 1,"+str(userInput))
+        print("with 1st method number of iteration is: "+str(iterationForPrime) +"\n")
+    else:
+        factorsOfCompositeNumber = ', '.join(map(str,factors))
+        print("\n"+ str(userInput)+" is a composite Number and factors are -> "+str(factorsOfCompositeNumber))
+        print("with 1st Method number of iteration is: "+str(iterationForComposite) + "\n")
+else:
+    print("Number should be in range of 2 and above")
