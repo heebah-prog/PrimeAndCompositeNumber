6f373633a33f9c9df7b4ff581e325d02c0aa92e4
heebah-prog <102908701+heebah-prog@users.noreply.github.com>
2022-05-23 18:07:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
+# Program to Calculate Prime Numbers and Composite Number
+# Program Get input from user
+# Then show how many iteration need to find out if number is Prime or not
+# If number is not prime, it displays the factor(s) of given number
+
+# Application Implement two methods
+# Method1: regular calculation Method
+# Method2: Use Algorithm
+
+import math
+
 
 def isInputValid(num):
     if(num <= 1):
@@ -9,49 +20,64 @@ def isInputValid(num):
 
 def primeCheckerMethod1(num):
     isPrime = False
-    iterationForPrime = 0
+    iteration = 0
 
+    if(num == 2): 
+        isPrime = True
     for x in range(2,num):
-        result = num%x
-        iterationForPrime += 1
-        if(result == 0):
+        iteration += 1
+        if(num%x == 0):
             isPrime = False
             break
         else:
             isPrime = True
 
-    return isPrime, iterationForPrime
+    return isPrime, iteration
+
+# Method 2 uses the Primality Test Algorithm
+def primeCheckerMethod2(n):
+    isPrime = False
+    iteration2 = 0
+    if n==1 or n==0 or (n%2 == 0 and n>2):
+        return isPrime, iteration2
+    else:
+        for i in range(3, int(math.sqrt(n))+1,2):
+            iteration2 += 1
+            if(n%i) == 0:
+                return isPrime, iteration2
+        isPrime = True
+        return isPrime, iteration2
 
 
 def getFactorsOfCompositeNumber(num):
     factors= []
-    iterationForComposite = 0
     for x in range(2, num):
-        iterationForComposite +=1
         if(num%x == 0):
             factors.append(x)
         else:
             pass
-    return factors, iterationForComposite
-
-
+    return factors
 
 # ***************************************
 #            APPLICATION ENTRY
 # ***************************************
 
+
 userInput = int(input("\nPlease give a number -> "))
 
-isPrime, iterationForPrime = primeCheckerMethod1(userInput)
-factors, iterationForComposite = getFactorsOfCompositeNumber(userInput)
+isPrime, iteration = primeCheckerMethod1(userInput)
+factors = getFactorsOfCompositeNumber(userInput)
+isPrime2, iteration2 = primeCheckerMethod2(userInput)
 
 if(isInputValid(userInput) != -1):
     if(isPrime == True):
         print("\n"+ str(userInput) +" is prime number and factors are -> 1,"+str(userInput))
-        print("with 1st method number of iteration is: "+str(iterationForPrime) +"\n")
+        print("with 1st method number of iteration is: "+str(iteration))
+        print("with 2nd method number of iteration is: "+str(iteration2) +"\n")
     else:
         factorsOfCompositeNumber = ', '.join(map(str,factors))
         print("\n"+ str(userInput)+" is a composite Number and factors are -> "+str(factorsOfCompositeNumber))
-        print("with 1st Method number of iteration is: "+str(iterationForComposite) + "\n")
+        print("with 1st Method number of iteration is: "+str(iteration))
+        print("with 2nd Method number of iteration is: "+str(iteration2) + "\n")
 else:
     print("Number should be in range of 2 and above")
